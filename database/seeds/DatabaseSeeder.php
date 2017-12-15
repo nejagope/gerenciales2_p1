0dcfb2c2ade3cdd409f6f72f30fedce88f43ff45
Nelson González <njgp86@gmail.com>
2017-12-15 11:41:06 -0600

    Se agregó el campo is_admin a la tabla de usuarios. Se agregó el usuario admin por defecto a las semillas de la base de datos. Issue #10

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+		DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+			'password' => bcrypt('admin'),
+			'is_admin' => true
+        ]);
     }
 }
