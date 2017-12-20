1d2057e73270989d76c06fa88bb4b317031845f7
Nelson González <njgp86@gmail.com>
2017-12-20 14:50:02 -0600

    Se completó el dashboard de Indicadores. Issue #9

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -23,32 +23,98 @@ class ReportsController extends Controller
 					->join('products', 'order_product.product_id', '=', 'products.id')
 					->join('categories', 'categories.id', '=', 'products.category_id')
 					->groupBy('categories.description')
-					->select(DB::raw('categories.description as category'), DB::raw('sum(products.price) as amount_ordered'))
+					->select(DB::raw('categories.description as category'), DB::raw('sum(products.price * order_product.amount) as amount_ordered'))
+					;
+					
+		//total de productos ordenados por categoría
+		$productos_ordenados = DB::table('orders')
+					->join('order_product', 'orders.id', '=', 'order_product.order_id')
+					->join('products', 'order_product.product_id', '=', 'products.id')
+					->join('categories', 'categories.id', '=', 'products.category_id')					
+					->groupBy('categories.description')
+					->select(DB::raw('categories.description as category'), DB::raw('sum(order_product.amount) as ordered_products'))
+					;
+					
+		$productos_devueltos = DB::table('devolutions')
+					->join('products', 'products.id', '=', 'devolutions.product_id')										
+					->join('categories', 'categories.id', '=', 'products.category_id')
+					->groupBy('categories.description')
+					->select(DB::raw('categories.description as category'), DB::raw('count(*) as devolutions'))
 					;
 		
 		$total_orders = DB::table('orders');
+		
 		$total_envios = DB::table('orders')
 							->join('shippings', 'orders.id', '=', 'shippings.order_id');
+		
+		$total_productos_ordenados = DB::table('orders')
+					->join('order_product', 'orders.id', '=', 'order_product.order_id')
+					->select(DB::raw('coalesce(sum(order_product.amount),0) as ordered_products'))
+					;
+					
+		$total_devoluciones = DB::table('orders')
+							->join('devolutions', 'orders.id', '=', 'devolutions.order_id');
+							
+				//total de productos ordenados por categoría
+		$top10_productos = DB::table('orders')
+					->join('order_product', 'orders.id', '=', 'order_product.order_id')
+					->join('products', 'order_product.product_id', '=', 'products.id')
+					->leftJoin('devolutions', 'products.id', '=', 'devolutions.product_id')										
+					->groupBy('products.name')													
+					->select(DB::raw('products.name as product'), DB::raw('coalesce(sum(order_product.amount),0) as ordered_products'))
+					->where('devolutions.id', null)
+					;
+					
+		$answers = DB::table('answers')
+						->join('questions', 'questions.id', '=', 'answers.question_id')
+						->groupBy('questions.question')
+						->select(DB::raw('questions.question as question'), DB::raw('coalesce(avg(answers.score),0) as score'));
 							
 		//aplicación de filtro de año a consultas en caso de que viniera el año  como parámetro en el request
 		if ($request->year){
 			$amounts_orders = $amounts_orders->whereBetween('orders.created_at', [$request->year. '-01-01', $request->year . '-12-31']);
 			
+			$productos_ordenados = $productos_ordenados->whereBetween('orders.created_at', [$request->year. '-01-01', $request->year . '-12-31']);
+			
 			$total_orders = $total_orders->whereBetween('orders.created_at', [$request->year. '-01-01', $request->year . '-12-31']);
 			
 			$total_envios = $total_envios->whereBetween('orders.created_at', [$request->year. '-01-01', $request->year . '-12-31']);
+			
+			$total_devoluciones = $total_devoluciones->whereBetween('devolutions.created_at', [$request->year. '-01-01', $request->year . '-12-31']);
+			
+			$total_productos_ordenados = $total_productos_ordenados->whereBetween('orders.created_at', [$request->year. '-01-01', $request->year . '-12-31']);
+			
+			$productos_devueltos = $productos_devueltos->whereBetween('devolutions.created_at', [$request->year. '-01-01', $request->year . '-12-31']);
+			
+			$top10_productos = $top10_productos->whereBetween('orders.created_at', [$request->year. '-01-01', $request->year . '-12-31']);
+			
+			$answers = $answers->whereBetween('answers.created_at', [$request->year. '-01-01', $request->year . '-12-31']);
 		}
 		
 		
 		$amounts_orders = $amounts_orders->get();
+		$productos_ordenados = $productos_ordenados->get();
+		$productos_devueltos = $productos_devueltos->get();	
+		$answers = $answers->get();
+		$top10_productos = $top10_productos->orderBy('ordered_products', 'desc')->get();	
+	
+		$total_productos_ordenados = $total_productos_ordenados->get();
+		$total_productos_ordenados = $total_productos_ordenados[0]->ordered_products;
 		$total_orders = $total_orders->count();
 		$total_envios = $total_envios->count();
+		$total_devoluciones = $total_devoluciones->count();
 		
 		$parametros = [
 			'years' => $years,
 			'amounts_orders' => $amounts_orders,
-			'total_orders' => $total_orders,
+			'total_orders' => $total_orders,			
 			'total_shippings' => $total_envios,
+			'ordered_products' => $productos_ordenados,
+			'total_ordered_products' => $total_productos_ordenados,
+			'total_devolutions' => $total_devoluciones,
+			'devolutions' => $productos_devueltos,
+			'top10_products' => $top10_productos,
+			'answers' => $answers,
 		];
         return view('reports.index', $parametros);
     }
--- resources/views/reports/index.blade.php
+++ resources/views/reports/index.blade.php
@@ -17,6 +17,11 @@
 			<li class="list-inline-item">
 				<h4>Años:</h4>
 			</li>
+			<li class="list-inline-item">
+				<a href="{{ route('reports_index') }}"> 
+				   Todos
+				</a>
+			</li>
 			@foreach ($years as $year)
 				<li class="list-inline-item">
 					<a href="{{ route('reports_index', ['year' => $year->year]) }}"> 
@@ -29,11 +34,17 @@
 		<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 		<script type="text/javascript">
 		  google.charts.load('current', {'packages':['corechart']});
+		  google.charts.load('current', {'packages':['table']});
+		  google.charts.load('current', {packages: ['corechart', 'bar']});
 		  google.charts.setOnLoadCallback(drawCharts);
 		  
 		  function drawCharts(){
 			  drawChartAmountsByCategory();
 			  drawChartOrdersShippings();
+			  drawChartProductsByCategory();
+			  drawChartProductsOrderedDevolutions();
+			  drawChartTop10Products();
+			  drawChartAnswers();
 		  }
 
 		  function drawChartAmountsByCategory() {
@@ -78,9 +89,146 @@
 
 			chart.draw(data, options);
 		  }
+		  
+		  function drawChartProductsByCategory() {
+
+			var data = google.visualization.arrayToDataTable([
+			  ['Categoría', 'Productos Ordenados', 'Productos Devueltos'],
+			  @foreach($ordered_products as $products)
+					[ '{{ $products->category }}' , {{ $products->ordered_products }}, 
+			  
+					@foreach ($devolutions as $devolution)
+					
+						@if ($devolution->category == $products->category)
+							{{ $devolution->devolutions }}  
+							@break
+						@endif
+						
+						@if ($loop->last)
+							0
+						@endif
+					@endforeach
+				  ],			  							 
+			  @endforeach
+			]);						
+
+			var options = {
+			  title: 'Productos ordenados y devueltos por categoría',
+			   hAxis: {
+				  title: 'Total Productos',
+				  minValue: 0
+				},
+				vAxis: {
+				  title: 'Categoría'
+				},
+				series: {
+				  0: {axis: 'Productos ordenados'},
+				  1: {axis: 'Productos devueltos'}
+				},
+				axes: {
+				  x: {
+					'Productos ordenados': {label: 'Productos ordenados', side: 'top'},
+					'Productos devueltos': {label: 'Productos devueltos'}
+				  }
+				}
+			};
+			
+			var chart = new google.visualization.BarChart(document.getElementById('chartOrderedProducts'));
+
+			chart.draw(data, options);
+		  }
+		  
+		  			
+		function drawChartProductsOrderedDevolutions() {
+
+			var data = google.visualization.arrayToDataTable([
+			  ['Estado', 'Total productos'],
+			  
+			  [ 'Productos Vendidos' , {{ $total_ordered_products - $total_devolutions }} ],
+			  [ 'Productos Devueltos' , {{ $total_devolutions }} ],
+			  
+			]);
+			
+			var options = {
+			  title: ' Prodductos vendidos vs Productos devueltos',
+			  is3D : true
+			};
+			
+			var chart = new google.visualization.PieChart(document.getElementById('chartSoldProducts'));
+
+			chart.draw(data, options);
+		  }
+		
+		
+		function drawChartTop10Products() {
+
+			var data = new google.visualization.DataTable();
+			data.addColumn('string', 'Product');
+			data.addColumn('number', 'Unidades vendidas');
+			
+			data.addRows(
+			[			  
+			  @foreach ($top10_products as $product)
+				['{{$product->product}}', {{$product->ordered_products}}],
+				@if ($loop->iteration == 10)
+					@break
+				@endif
+			  @endforeach
+			  
+			]);
+			
+			var options = {
+			  title: 'Top 10 Prodductos más vendidos',
+			  showRowNumber: true, 
+			  width: '60%', 
+			  height: '100%'			  
+			};
+			
+			var chart = new google.visualization.Table(document.getElementById('chartTop10Products'));
+
+			chart.draw(data, options);
+		  }
+		  
+		  function drawChartAnswers() {
+
+			var data = new google.visualization.DataTable();
+			data.addColumn('string', 'Pregunta');
+			data.addColumn('number', 'Respuesta Promedio');
+			data.addRows([			  
+			  @foreach($answers as $answer)
+		  ['{{$answer->question}}' , {{$answer->score}}],
+			  @endforeach
+			]);
+			
+			var options = {
+			    title: 'Resultado de encuestas',	
+				hAxis:{
+					title: 'Pregunta'
+				},
+				vAxis:{
+					title: 'Respuesta (1-5)',
+					viewWindow:{
+						min:0,
+						max:6
+					}
+				}
+			};
+			
+			var chart = new google.visualization.ColumnChart(document.getElementById('chartAnswers'));
+
+			chart.draw(data, options);
+		  }
 		</script>
+		
 		<div id="amountsCategories" style="width: 100%; height: 500px;"></div>
 		<div id="chartOrdersShippings" style="width: 100%; height: 500px;"></div>
+		<div id="chartOrderedProducts" style="width: 100%; height: 500px;"></div>
+		<div id="chartSoldProducts" style="width: 100%; height: 500px;"></div>
+		<h3 class="text-center">Top 10 productos vendidos</h3>
+		<div id="chartTop10Products" style="width: 100%; height: 500px;" class="text-center"></div>
+		<div id="chartAnswers" style="width: 100%; height: 500px;"></div>
+		<div>
+		
 		</div>
 		
 	</div>
